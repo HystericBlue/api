29baa65cf7152af97c471483b6b3bbbfc95c74bf
moodopa <egglovery@gmail.com>
2019-08-07 16:19:18 +0900

    first created
    2019.08.07 wn.kim created

---
+++ backend/api/src/main/java/com/moodopa/pricone/PriconeApplication.java
@@ -0,0 +1,13 @@
+package com.moodopa.pricone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriconeApplication {
+
+public static void main( String[] args ) {
+	SpringApplication.run( PriconeApplication.class, args );
+}
+
+}
---
+++ backend/api/src/test/java/com/moodopa/pricone/PriconeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.moodopa.pricone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith( SpringRunner.class )
+@SpringBootTest
+public class PriconeApplicationTests {
+
+@Test
+public void contextLoads( ) {
+}
+
+}
