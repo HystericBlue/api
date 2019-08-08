b7c1ad86c38f18cda8858226d48b0ed61e3ce56b
moodopa <egglovery@gmail.com>
2019-08-08 11:33:00 +0900

    2019.08.08 init 호출 부분 수정

--- backend/scraping/testcode/webScrapingK.py
+++ backend/scraping/testcode/webScrapingK.py
@@ -1,7 +1,6 @@
 import webScraping
 from bs4 import BeautifulSoup
-headers = {}
-webscrraping = webScraping.init(headers)
+webscrraping = webScraping.init({})
 resdata = webscrraping.targetsite('https://gall.dcinside.com/mgallery/board/view/?id=purikone_redive&no=1097025', 'get')
 print('resdata.code : ', resdata.get('code'))
 soup = BeautifulSoup(resdata.get('data'), 'html.parser')
