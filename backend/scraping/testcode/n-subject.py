d68341fd39205ba68016a0d42060004daeebf988
HystericBlue <53254867+HystericBlue@users.noreply.github.com>
2019-08-12 12:13:07 +0900

    Update n-subject.py
    1차 수정

--- backend/scraping/testcode/n-subject.py
+++ backend/scraping/testcode/n-subject.py
@@ -1,18 +1,77 @@
 from bs4 import BeautifulSoup
-import requestURL
+import webScraping
+import re
+webscrraping = webScraping.init({})
+resdata = webscrraping.targetsite('https://gall.dcinside.com/mgallery/board/lists?id=purikone_redive&exception_mode=recommend', 'get')
+soup = BeautifulSoup(resdata.get('data'), 'html.parser')
+trdata = soup.select('tr.ub-content.us-post')
+for tr in trdata:
+    gallNum = tr.select('td.gall_num')
+    gallNum = str(gallNum)
+    gallNum = (re.sub('<.+?>', '', gallNum, 0).strip())
+    gallSubject = tr.select('td.gall_subject')
+    gallSubject = str(gallSubject)
+    gallSubject = (re.sub('<.+?>', '', gallSubject, 0).strip())
+    gallTit = tr.select('td.gall_tit > a')
+    gallTit = str(gallTit)
+    gallTit = (re.sub('<.+?>', '', gallTit, 0).strip())
+    gallTiturl = tr.select('td.gall_tit > a')
+    gallTiturl = gallTiturl[0].get('href')
+    gallWriter = tr.select('td.gall_writer > span')
+    gallWriter = str(gallWriter)
+    gallWriter = (re.sub('<.+?>', '', gallWriter, 0).strip())
+    gallDate = tr.select('td.gall_date')
+    gallDate = str(gallDate)
+    gallDate = (re.sub('<.+?>', '', gallDate, 0).strip())
 
-url = 'https://gall.dcinside.com/mgallery/board/view/?id=purikone_redive&no=1097025'
-headers = {}
-data = {}
 
-res = requests.get(URL, headers=headers, data=data)
-html = res.text
-soup = BeautifulSoup(html, 'html.parser')
-post = soup.select('tr.ub-content.us-post')
-i = 0
-for trHtml in post:
-    # print(trHtml)
-    tr = trHtml.select('td')
-    for td in tr:
-        i = i+1
-        print(i, ":", td)
+    print(gallNum)
+    print(gallSubject)
+    print(gallTit)
+    print(gallTiturl)
+    print(gallWriter)
+    print(gallDate)
+
+
+#    tddata = tr.select('td.gall_num,td.gall_subject,td.gall_tit,td.gall_writer.ub-write,td.gall_date')
+    """
+    tddata = str(trdata.select('td.gall_num,.td.gall_subject,td.gall_tit.ub-word,td.gall_writer ub-write,td.gall_date'))
+    tddata = re.sub('<.+?>', '', tddata, 0).strip()
+    print(tddata)
+    """
+#tddata = str(tddata)
+#print(tddata)
+"""
+    for td in tddata:
+        td = str(td)
+        if td.find('gall_num') != -1:
+            gallNum = (re.sub('<.+?>', '', td, 0).strip())
+            print('gall_num : ', gallNum)
+        else:
+            if td.find('gall_subject') != -1:
+                gallSubject = (re.sub('<.+?>', '', td, 0).strip())
+                print('gall_subject : ', gallSubject)
+            else:
+                if td.find('gall_tit') != -1:
+                    gallTit = td
+                    print(gallTit)
+                    gallTit = gallTit.get('href')
+                    print('gall_tit : ', gallTit)
+                else:
+                    if td.find('td.gall_writer.ub-write') != -1:
+                        gallWriter = (re.sub('<.+?>', '', td, 0).strip())
+                        print('gall_writer : ', gallWriter)
+                    else:
+                        if td.find('td.gall_date') != -1:
+                            gallDate = (re.sub('<.+?>', '', td, 0).strip())
+                            print('gall_writer : ', gallDate)
+                        else:
+                            print('gall_num None')
+"""
+
+#for trHtml in post:
+#    tr = trHtml.select('td')
+#    for td in tr:
+#        print(":", td)
+
+#""", gallCategory, gallSubject, gallSubjectUrl, nickname, date"""
