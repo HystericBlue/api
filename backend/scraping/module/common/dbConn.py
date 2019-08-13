46383f18d9d20712c9b0729db31e3d3d6222df27
moodopa <egglovery@gmail.com>
2019-08-13 10:26:54 +0900

    2019.08.13 dbconn 부분 수정

--- backend/scraping/module/common/dbConn.py
+++ backend/scraping/module/common/dbConn.py
@@ -2,19 +2,19 @@ import pymysql
 
 
 # dbConn.py
-class init:
+class ListTableInit:
     def __init__(self, host, port, user, password, db):
         self.conn = pymysql.connect(host=host, port=port, user=user, password=password, db=db, charset='utf8')
         self.cursor = self.conn.cursor()
 
-    def gallnum_check(self, param):
+    def numcheck(self, param):
         sql = 'SELECT COUNT(1) FROM TARGET_LIST WHERE 1 = 1 and TARGET_SEQ = %s'
         self.cursor.execute(sql, param)
         row = self.cursor.fetchone()
-        self.conn.close()
         return row[0]
 
-    def insert_list(self, param):
+    def insert(self, param):
+        print('param : ', param)
         sql = 'INSERT INTO TARGET_LIST (TARGET_SEQ, TARGET_SITE_TYPE, TARGET_CNTN_TYPE, TARGET_CNTN_URL, TARGET_TITLE, REG_ID, REG_DATE) VALUES (%s, %s, %s, %s, %s, %s, %s)'
         self.cursor.execute(sql, param)
         self.conn.commit()
