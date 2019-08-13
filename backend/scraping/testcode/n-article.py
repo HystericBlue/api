75dfbf8e3430b4f3df64427f9daa38473d87acbe
HystericBlue <53254867+HystericBlue@users.noreply.github.com>
2019-08-13 14:07:45 +0900

    Update n-article.py
    class 화

--- backend/scraping/testcode/n-article.py
+++ backend/scraping/testcode/n-article.py
@@ -1,29 +1,28 @@
 from bs4 import BeautifulSoup
-import requests
-URL = 'https://gall.dcinside.com/mgallery/board/view/?id=purikone_redive&no=1097025'
+import webScraping
+import re
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3'
-}
 
-data = {}
+class ArticleScr:
+    def init(self, gallNum, gallTiturl, count=0, flags=0):
+        self.gallNum = gallNum
+        self.gallTiturl = gallTiturl
+        self.count = count
+        self.flags = flags
 
-res = requests.get(URL, headers=headers, data=data)
-html = res.text
-soup = BeautifulSoup(html, 'html.parser')
-post = soup.select('div.view_content_wrap')
+    def articleParser(self):
+        webscrraping = webScraping.init({})
+        resdata = webscrraping.targetsite(self.gallTiturl, 'get')
+        soupB = BeautifulSoup(resdata.get('data'), 'html.parser')
+        postArticle = soupB.select('div.view_content_wrap')
+        for divHtml in postArticle:
+            category = divHtml.select('h3.title.ub-word > span.title_headtext')
+            subject = divHtml.select('h3.title.ub-word > span.title_subject')
+            article = divHtml.select('div.writing_view_box > div> div')
+            self.articleMap = {'category':category, 'subject':subject, 'article':article }
+        return self.articleMap
 
-for divHtml in post:
-    i = 0
-    category = divHtml.select('h3.title.ub-word > span.title_headtext')
-    subject = divHtml.select('h3.title.ub-word > span.title_subject')
-    article = divHtml.select('div.writing_view_box > div')
-    articleIa = divHtml.select('div.writing_view_box > div > div > img')
-    articleText = article[1].text.strip()
-    articleImage = articleIa[0].get('src')
-    print("category :", category)
-    print("subject :", subject)
-    print("article text :", articleText)
-    print("article img :", articleImage)
-    print("article raw :", article[1])
+#    def converter(self):
+#        resdata = str(self.articleMap[])
+#        resdata = (re.sub(self.gallNum, self.gallTiturl, resdata, self.count).strip())
+#        return resdata
