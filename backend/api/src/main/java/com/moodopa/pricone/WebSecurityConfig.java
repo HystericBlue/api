9e4b269717f5713a761a400e929581e538f71047
moodopa <egglovery@gmail.com>
2019-08-13 13:58:13 +0900

    2019.09.13 wn.kim add
    기초저긴 프로젝트

---
+++ backend/api/src/main/java/com/moodopa/pricone/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.moodopa.pricone;
+
+// @Configuration
+// @EnableWebMvcSecurity
+// @EnableGlobalMethodSecurity(prePostEnabled = true)
+// public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
+	// @Override
+	// protected void configure(HttpSecurity http) throws Exception {
+	// 	http
+	// 			.authorizeRequests()
+	// 			.antMatchers("/", "/js/**", "/css/**", "/fonts/**", "/user/signup", "/api/**").permitAll() /*로그인 없이 접속 할 수 있는 위치*/
+	// 			.anyRequest().authenticated();
+	// 	http
+	// 			.formLogin()
+	// 			.loginPage("/login")
+	// 			.defaultSuccessUrl("/")
+	// 			.permitAll()
+	// 			.and()
+	// 			.logout()
+	// 			.permitAll();
+	// 	
+	// }
+}
