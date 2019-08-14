b972dc99ced85c139fa51533bfb6653cba36bfdf
moodopa <egglovery@gmail.com>
2019-08-14 11:56:25 +0900

    2019.08.14 wn.kim noneValid 제거

--- backend/scraping/module/common/dbConn.py
+++ backend/scraping/module/common/dbConn.py
@@ -23,17 +23,8 @@ class ListTableInit:
 
 class CntnTableInit:
     def __init__(self, host, port, user, password, db):
-        params = {'host': host, 'port': port, 'user': user, 'password': password, 'db': db}
-        print('listTableInit params: ', params)
-        paramvalid = noneValid.DBConnValid(params)
-        retmap = paramvalid.valid()
-        print('valid res : ', retmap)
-        if retmap.get('code') != 200:
-            return retmap
-        retmap = {'code': 200, 'message': '정상적으로 처리되었습니다.'}
         self.conn = pymysql.connect(host=host, port=port, user=user, password=password, db=db, charset='utf8')
         self.cursor = self.conn.cursor()
-        return retmap
 
     def getlistseq(self, param):
         sql = 'SELECT LIST_SEQ FROM TARGET_LIST WHERE 1 = 1 and TARGET_SEQ = %s'
